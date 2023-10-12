d7e7c5ff7181b241f4069d2c417248ca0cd743e6
BOUISK ACHRAF <abouisk@courdescomptes.ma>
2023-10-12 12:43:55 +0100

    react-native-jobs

---
+++ hook/useFetch.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+// import { RAPID_API_KEY } from "@env";
+
+// const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "bb8bf52a33msh72c0ba84d55cdcep1cc04ejsn1498db567944",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
